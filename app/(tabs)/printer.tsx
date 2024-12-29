68d3a469e7ac9cec85275d20e9a605fafbfe3802
SyntaxLexx <spencermainaa@gmail.com>
2024-12-29 10:57:45 +0300

    installed nativeWind

--- app/(tabs)/printer.tsx
+++ app/(tabs)/printer.tsx
@@ -1,12 +1,15 @@
+import { Button } from "@/components/Button";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Printer = () => {
   return (
     <SafeAreaView>
       <ThemedView style={styles.container}>
         <ThemedText type="title">Printer Testing</ThemedText>
+        <Button label="Click Me"></Button>
       </ThemedView>
     </SafeAreaView>
   );
